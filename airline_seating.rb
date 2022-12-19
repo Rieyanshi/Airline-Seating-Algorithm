e1fb4b10853a48afdef11e25f01560834b594c5c
Rieyanshi Sharma <86876634+Rieyanshi@users.noreply.github.com>
2022-12-19 21:48:58 +0530

    Update airline_seating.rb

--- airline_seating.rb
+++ airline_seating.rb
@@ -2,127 +2,127 @@
 require 'json'
 
 class AirlineSeating
-  attr_accessor :excess_booking, :max_seats, :passengers_count, :message
+  attr_accessor :passengers_seats, :seatCount, :passengers, :status
 
   def initialize(*args)
-    @message = validate_input(args)
-    if @message.nil?
-      @max_seats = @input_seats.inject(0) { |sum, x| sum += x[0] * x[1] }
-      @excess_booking = true if @max_seats < @passengers_count
-      @max_columns = @input_seats.map(&:last).max
-      @passengers_allocated = 0
+    @status = constraint_check(args)
+    if @status.nil?
+      @seatCount = @seats_available.inject(0) { |sum, x| sum += x[0] * x[1] }
+      @passengers_seats = true if @seatCount < @passengers
+      @max_columns = @seats_available.map(&:last).max
+      @Completion_count = 0
     end
   end
 
   def arrangement
-    prepare_seats
-    allocate_aisle_seats
-    allocate_window_seats
-    allocate_center_seats
+    array_formation
+    aisle_seat_completion
+    window_seat_completion
+    center_seat_completion
   end
 
-  def validate_input(args)
-    return "Please provide input: A 2D array and the Number of passengers!" if args.flatten.map(&:strip).reject(&:empty?).empty?
-    return "Please provide a 2D array as the first argument!" if args.flatten[0].strip.empty?
-    return "Please provide the Number of passengers as the second argument!" if args.flatten[1].nil? || args.flatten[1].strip.empty?
+  def constraint_check(args)
+    return "Enter 2D array and number of passenger as input!" if args.flatten.map(&:strip).reject(&:empty?).empty?
+    return "Enter 2D array" if args.flatten[0].strip.empty?
+    return "Enter Number of passengers" if args.flatten[1].nil? || args.flatten[1].strip.empty?
     begin
-      @input_seats = JSON.parse(args.flatten[0])
+      @seats_available = JSON.parse(args.flatten[0])
     rescue
-      return "The first argument is invalid! Please input a 2D array."
+      return "Array formating issue: Array is not entered correctly"
     end
-    return "The given array is not in 2D format!" unless @input_seats.all? { |x| x.is_a?(Array) }
-    return "All the sub-arrays of the given 2D array are not of [x,y] format!" unless @input_seats.all? { |x| x.size == 2 }
-    return "The sub-arrays are in [x,y] format but 'x' and 'y' should be NON-ZERO values!" if @input_seats.any? { |x| x.any?(0) }
+    return "Please enter 2D format array" unless @seats_available.all? { |x| x.is_a?(Array) }
+    return "Subarray entered are not valid" unless @seats_available.all? { |x| x.size == 2 }
+    return "Values entered in array are incorrect" if @seats_available.any? { |x| x.any?(0) }
     begin
-      @passengers_count = JSON.parse(args.flatten[1])
+      @passengers = JSON.parse(args.flatten[1])
     rescue
-      return "The second argument is invalid! Please enter the number of passengers."
+      return "No. of passenger is not correct: please enter a valid integer"
     end
-    return "The second argument should be a positive integer" unless @passengers_count.is_a?(Integer)
+    return "Ener a positive integer only" unless @passengers.is_a?(Integer)
   end
 
   private
 
-  def prepare_seats
-    @available_seats = @input_seats.each_with_object([]).with_index do |(arr, seats), index|
+  def array_formation
+    @empty_seat = @seats_available.each_with_object([]).with_index do |(arr, seats), index|
       seats << (1..arr[1]).map { |x| Array.new(arr[0]) { 'N' } }
     end
     @sorted_seats = (1..@max_columns).each_with_object([]).with_index do |(x, arr), index|
-      arr << @available_seats.map { |x| x[index] }
+      arr << @empty_seat.map { |x| x[index] }
     end
   end
 
-  def allocate_aisle_seats
-    @aisle_seats = @sorted_seats.each_with_object([]) do |elem_array, res_array|
+  def aisle_seat_completion
+    @corner seats = @sorted_seats.each_with_object([]) do |elem_array, res_array|
       res_array << if elem_array.nil?
         nil
       else
-        elem_array.each_with_object([]).with_index do |(basic_elem_array, update_arr), index|
-          update_arr << if basic_elem_array.nil?
+        elem_array.each_with_object([]).with_index do |(sitting_arrangement, update_arr), index|
+          update_arr << if sitting_arrangement.nil?
             nil
           else
             if index == 0
-              @passengers_allocated += 1
-              basic_elem_array[-1] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
+              @Completion_count += 1
+              sitting_arrangement[-1] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
             elsif index == elem_array.size - 1
-              unless basic_elem_array.size == 1
-                @passengers_allocated += 1
-                basic_elem_array[0] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
+              unless sitting_arrangement.size == 1
+                @Completion_count += 1
+                sitting_arrangement[0] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
               end
             else
-              @passengers_allocated += 1
-              basic_elem_array[0] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
-              unless basic_elem_array.size == 1
-                @passengers_allocated += 1
-                basic_elem_array[-1] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
+              @Completion_count += 1
+              sitting_arrangement[0] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
+              unless sitting_arrangement.size == 1
+                @Completion_count += 1
+                sitting_arrangement[-1] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
               end
             end
-            basic_elem_array
+            sitting_arrangement
           end
         end
       end
     end
   end
 
-  def allocate_window_seats
-    @window_seats = @aisle_seats.each_with_object([]) do |elem_array, res_array|
+  def window_seat_completion
+    @window_seats = @corner seats.each_with_object([]) do |elem_array, res_array|
       res_array << if elem_array.nil?
         nil
       else
-        elem_array.each_with_object([]).with_index do |(basic_elem_array, update_arr), index|
-          update_arr << if basic_elem_array.nil?
+        elem_array.each_with_object([]).with_index do |(sitting_arrangement, update_arr), index|
+          update_arr << if sitting_arrangement.nil?
             nil
           else
             if index == 0
-              @passengers_allocated += 1
-              basic_elem_array[0] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
+              @Completion_count += 1
+              sitting_arrangement[0] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
             elsif index == elem_array.size - 1
-              @passengers_allocated += 1
-              basic_elem_array[-1] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
+              @Completion_count += 1
+              sitting_arrangement[-1] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
             end
-            basic_elem_array
+            sitting_arrangement
           end
         end
       end
     end
   end
 
-  def allocate_center_seats
+  def center_seat_completion
     @center_seats = @window_seats.each_with_object([]) do |elem_array, res_array|
       res_array << if elem_array.nil?
         nil
       else
-        elem_array.each_with_object([]).with_index do |(basic_elem_array, update_arr), index|
-          update_arr << if basic_elem_array.nil?
+        elem_array.each_with_object([]).with_index do |(sitting_arrangement, update_arr), index|
+          update_arr << if sitting_arrangement.nil?
             nil
           else
-            if basic_elem_array.size > 2
-              (1..basic_elem_array.size - 2).each do |x|
-                @passengers_allocated += 1
-                basic_elem_array[x] = @passengers_allocated <= @passengers_count ? @passengers_allocated.to_s.rjust(@max_seats.to_s.size, "0") : 'X'*@max_seats.to_s.size
+            if sitting_arrangement.size > 2
+              (1..sitting_arrangement.size - 2).each do |x|
+                @Completion_count += 1
+                sitting_arrangement[x] = @Completion_count <= @passengers ? @Completion_count.to_s.rjust(@seatCount.to_s.size, "0") : 'X'*@seatCount.to_s.size
               end
             end
-            basic_elem_array
+            sitting_arrangement
           end
         end
       end
@@ -137,9 +137,9 @@ lines = File.readlines('input.txt')
 # pass_count = gets.chomp
 # seating = AirlineSeating.new(array_ip, pass_count)
 seating = AirlineSeating.new(lines)
-if seating.message.nil?
-  puts "Sorry! We don't have enough seats to occupy #{seating.passengers_count} passengers.\
-   Only #{seating.max_seats} seats are available!" if seating.excess_booking
+if seating.status.nil?
+  puts "Number of seats are less than passenger count : #{seating.passengers} .\
+   Only #{seating.seatCount} seats are available!" if seating.passengers_seats
   result = seating.arrangement
   File.open("output.txt", "w") do |file|
     result.each_with_index do |row, parent_index|
@@ -161,5 +161,5 @@ if seating.message.nil?
     end
   end
 else
-  puts seating.message
+  puts seating.status
 end
